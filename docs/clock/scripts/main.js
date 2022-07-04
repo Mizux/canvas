d69dad1fdcaea9222df4c685058ca605161fcdc7
Corentin Le Molgat <corentinl@google.com>
2022-07-04 09:21:49 +0200

    clock: add Stats.js

--- docs/clock/scripts/main.js
+++ docs/clock/scripts/main.js
@@ -1,5 +1,9 @@
 import { default as dat } from "./dat.gui.module.js";
 
+const stats = new Stats();
+stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild( stats.dom );
+
 // Color Structure
 var blob = {
   seed: 3,
@@ -131,6 +135,8 @@ gui.close();
 var vertices;
 
 function update() {
+	stats.begin();
+
   PRNG.setSeed(blob.seed);
 
   var r, a, v, o;
@@ -176,6 +182,8 @@ function update() {
     ctx.globalAlpha = (1 - i/m) * .1;
     yolo(vertices, i, size, size);
   }
+
+  stats.end();
   requestAnimationFrame(update);
 }
 
--- docs/clock/scripts/update_deps.sh
+++ docs/clock/scripts/update_deps.sh
@@ -4,6 +4,8 @@ set -eux
 # Check wget is available
 command -v wget
 
+wget -N --trust-server-name https://unpkg.com/stats-js/build/stats.js
+
 #wget -N --trust-server-name https://unpkg.com/dat.gui/build/dat.gui
 wget -N --trust-server-name https://unpkg.com/dat.gui/build/dat.gui.module
 wget -N --trust-server-name https://unpkg.com/dat.gui/build/dat.gui.module.js.map
